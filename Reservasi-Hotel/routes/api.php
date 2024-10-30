4fe94b9034382c64a0959a6512f15a7bff590b68
marselyna <161105972+marselyna@users.noreply.github.com>
2024-10-30 14:28:14 +0700

    api

--- Reservasi-Hotel/routes/api.php
+++ Reservasi-Hotel/routes/api.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('reservasi', [ReservasiController::class, 'getReservasi']);
+Route::post('reservasi', [ReservasiController::class, 'storeReservasi']);
